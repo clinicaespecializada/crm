6d02ecc6611179bd85b5cb60f74bd92b38bbd1bf
clinicaespecializada <administrativo@clinicaespecializada.com.br>
2024-09-12 15:33:40 -0300

    first commit

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n^_tsy5d-_l*0s0x9n6v7%&+his%+y9ut023vv+^df!)x2mw7l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['92.113.32.67']
 
 
 # Application definition
